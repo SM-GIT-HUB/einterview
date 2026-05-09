46bea998f4003c291933c0b695c50da0d921de94
Soumik Majumder <soumikcode02@gmail.com>
2026-05-09 16:55:21 +0530

    fix env issue

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,12 +1,12 @@
 import nodemailer from "nodemailer"
 
 export const transporter = nodemailer.createTransport({
-    host: SMTP_HOST,
-    port: SMTP_PORT,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
     secure: false,
     auth: {
-        user: SMTP_USER,
-        pass: SMTP_PASS,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
     }
 })
 
