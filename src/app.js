8bb71ebb18412e16882788650f619db69b72a090
Soumik Majumder <soumikcode02@gmail.com>
2026-05-09 23:39:55 +0530

    FIX: allow all urls for dev

--- src/app.js
+++ src/app.js
@@ -9,8 +9,13 @@ import roomRoutes from "./routes/room-routes.js"
 
 const app = express();
 
+// app.use(cors({
+//     origin: process.env.CLIENT_URL,
+//     credentials: true
+// }))
+
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: true,
     credentials: true
 }))
 
